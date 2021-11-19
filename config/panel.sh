80c02363c049cad61312b79f817fddf4693cf91e
ZorBott <81800492+ZorBott@users.noreply.github.com>
2021-11-19 14:37:57 +0100

    Add files via upload

---
+++ config/panel.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+XPOS=0
+WIDTH=1920
+HEIGHT=16
+FONT="terminus-12"
+FG="#575B70"
+BG="#131C26"
+
+GREEN="^fg(#5AF78E)^bg()"
+YELLOW="^fg(#F4F99D)^bg()"
+RED="^fg(#FF6E67)^bg()"
+WHITE="^fg(#E6E6E6)^bg()"
+PINK="^fg(#FF92D0)^bg()"
+BLUE="^fg(#9AEDFE)^bg()"
+MAGENTA="^fg(#CAA9FA)^bg()"
+
+
+CLEAN="^fg()^bg()"
+
+while true; do
+
+	XWINDOW=$(echo "Active Window: `echo $(xdotool getwindowfocus getwindowname)`")
+	TIME=$(echo "Date: `echo $(date +"%a %d %B at %R")`")
+	BATTERYLINE=$(echo "Battery: `cat /sys/class/power_supply/BAT0/capacity `%")		
+	KERNEL=$(echo "Kernel: `echo $(uname -r)`")
+	UPDATES=$(echo "Updates: `echo $(checkupdates | wc -l)`")
+	#AURUPDATES=$(echo "Aur: `echo $(yay -Qau | wc -l)`")
+	WIFILINE=$(echo "WiFi: `echo $(iw dev wlan0 info | grep ssid | awk '{print $2}')`")
+
+	xsetroot -name "  $BATTERYLINE  |  $TIME  |  $KERNEL  |  $UPDATES  |  $WIFILINE  "
+
+	sleep 1	
+
+done 
+
