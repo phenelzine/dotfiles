8f7e6d916c41f48cfae67cd22da4096993a97d6f
mihail-bogdanov <81800492+mihail-bogdanov@users.noreply.github.com>
2021-11-23 15:35:16 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+# install yay for it to work #
+echo "yay must be installed"
+
+yay --needed --ask 4 -Sy - < packages.txt
+
+sudo mkdir ~/.config
+sudo mkdir ~/.config/openbox
+sudo mkdir ~/.config/tint2
+sudo mkdir ~/.config/dunst
+
+echo " now copy the files in the directories yourself"
+
