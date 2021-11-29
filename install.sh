9c9a52a342a606be2c889922261613ddd3f80e24
mihail-bogdanov <mihabog0720@gmail.com>
2021-11-29 11:08:39 +0100

    added wallpaper and updated install

--- install.sh
+++ install.sh
@@ -11,11 +11,12 @@ sudo mkdir /usr/share/backgrounds/
 
 echo " copying files..."
 
+sudo mv wallpaper.jpg /usr/share/backgrounds/
 mv ratpoison/ratpoisonrc ~/.ratpoisonrc
 mv openbox/ ~/.config/openbox
 sudo mv wallpapers/* /usr/share/backgrounds/
 rm README.md
-mv config/* ~/.config
+mv config/* ~/.config/
 mv starship.toml ~/.config/
 mv .zshrc ~/.zshrc
 mv .bashrc ~/.bashrc
