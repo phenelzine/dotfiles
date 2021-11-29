4e2b5b84981d5e82a74b45c784b855c3ee7ce3c1
mihail-bogdanov <mihabog0720@gmail.com>
2021-11-29 10:53:04 +0100

    updated panel

--- config/panel.sh
+++ config/panel.sh
@@ -25,10 +25,10 @@ while true; do
 	BATTERYLINE=$(echo "Battery: `cat /sys/class/power_supply/BAT0/capacity `%")		
 	KERNEL=$(echo "Kernel: `echo $(uname -r)`")
 	UPDATES=$(echo "Updates: `echo $(checkupdates | wc -l)`")
-	#AURUPDATES=$(echo "Aur: `echo $(yay -Qau | wc -l)`")
+	UPTIME=$(echo "Uptime: `echo $(uptime -p)`")
 	WIFILINE=$(echo "WiFi: `echo $(iw dev wlan0 info | grep ssid | awk '{print $2}')`")
 
-	xsetroot -name "  $BATTERYLINE  |  $TIME  |  $KERNEL  |  $UPDATES  |  $WIFILINE  "
+	xsetroot -name "  $BATTERYLINE  |  $TIME  |  $KERNEL  |  $UPDATES  |  $WIFILINE  |  $UPTIME "
 
 	sleep 1	
 
