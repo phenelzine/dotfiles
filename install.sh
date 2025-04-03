7aea76e9508e9d780ef4ef24333cd9468127343c
phenelzine <phenelzine@alphazero.homenet.telecomitalia.it>
2025-04-03 23:28:20 +0200

    modificationz

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# install yay for it to work #
-echo "yay must be installed"
-echo "network manager must be enabled"
 echo "run as root"
-
-yay --needed --ask 4 -Sy - < packages.txt
 
 mkdir ~/.config/
 mkdir /usr/share/backgrounds/
@@ -26,6 +21,4 @@ mv zshrc ~/.zshrc
 mv bashrc ~/.bashrc
 mv pelangi-noita/ ~/.themes/
 mv Noita/ ~/.themes/
-systemctl enable ly
-systemctl enable plymouth
 
